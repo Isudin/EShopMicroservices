1736579fb45928ce4a1fe5fb6b5bae65d0cbf108
Isu <natantrojak@wp.pl>
2024-04-02 21:00:25 +0200

    Added Marten service

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,12 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("CatalogApi")!);
+    if (builder.Environment.IsDevelopment())
+        options.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All;
+});
 
 var app = builder.Build();
 
